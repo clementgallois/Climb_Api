44df135875be5840a5ac5a72e611e8c83c82d15f
Jessy Hiroquoy <hiroquoy.jessy@gmail.com>
2017-09-15 18:23:33 +0200

    Better imagedefault

--- app/models/user.js
+++ app/models/user.js
@@ -44,7 +44,7 @@ userSchema.methods.safeUser = (user) => {
       username : user.profile.username,
       firstName : user.profile.firstName,
       lastName : user.profile.lastName,
-      pictureUrl : user.profile.pictureUrl || user.profile."imagedefault.png",
+      pictureUrl : user.profile.pictureUrl || "imagedefault.png",
       gender : user.profile.gender,
       bio : user.profile.bio,
       verified : user.profile.verified
